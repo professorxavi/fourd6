1120397fe0905861852458707fa2f78bbed324eb
X <xavier@tauntaun.net>
2019-02-05 21:35:54 -0600

    Update app and index with router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import { Button, Popover, Menu, MenuItem, Position } from "@blueprintjs/core";
 import { Header } from './components/header';
 import { AbilityScores } from './components/stat-roller';
+import { Class } from './components/class-survey';
+import { Route } from 'react-router-dom';
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+
   render() {
+    const content =
+      <Menu>
+        <MenuItem href="/" icon="new-person" text="Roller"/>
+        <MenuItem href="/class" icon="diagram-tree" text="Lvl 0"/>
+        <MenuItem href="/class" icon="crown" text="DM Tools"/>
+        <MenuItem href="/class" icon="grid" text="Table"/>
+      </Menu>;
+
     return (
       <div className="app">
-        <div className="bp3-dark">
-          <Header />
-          <div>
-            <img src={logo} className="logo" alt="logo" />
-            <AbilityScores />
+          <div className="bp3-dark">
+            <Header />
+            <div className="container">
+              <Route exact path="/" component={AbilityScores} />
+              <Route path="/class" component={Class} />
+            </div>
+            <div className="footer">
+              <div className="button-div">
+                <Popover content={content} position={Position.TOP}>
+                    <Button id="nav-button" icon="more" />
+                </Popover>
+              </div>
+              <img src={logo} className="logo" alt="logo" />
+            </div>
           </div>
-        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -8,7 +9,13 @@ import 'normalize.css/normalize.css';
 import '@blueprintjs/icons/lib/css/blueprint-icons.css';
 import '@blueprintjs/core/lib/css/blueprint.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+    <App />
+  </Router>
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
