1651a03353428f5a44c9bb43b0e3a080a6b7ff8f
X <xavier@tauntaun.net>
2019-02-05 21:37:46 -0600

    initial class survey component

---
+++ src/components/class-survey/Class.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Class extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Words and shit</p>
+      </div>
+    );
+  }
+}
+
+export default Class;
---
+++ src/components/class-survey/index.js
@@ -0,0 +1,3 @@
+import Class from './Class';
+
+export { Class };
